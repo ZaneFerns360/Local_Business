63ea4f3e3a914cbf83ece57972ff211eaa1a200c
Zane Fernandes <zane.ferns360@gmail.com>
2023-09-09 13:30:56 +0530

    Fixed small error

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        satoshi: ['"Satoshi"', 'sans-serif'],
+        inter: ['"Inter"', 'sans-serif'],
+        poppins: ['"Poppins"', 'sans-serif'],
+      },
     },
   },
   plugins: [],
